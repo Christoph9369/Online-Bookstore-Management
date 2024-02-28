343ad270403be47801c6b02b03de9141de5cdfb3
Christoph9369 <mojuyefelix@gmail.com>
2024-02-28 19:11:48 +0200

    adding react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBooks from "./pages/CreateBooks";
+import DeleteBooks from "./pages/DeleteBooks";
+import EditBook from "./pages/EditBook";
+import Showbooks from "./pages/Showbooks";
 
 const App = () => {
-  return <h1 className="bg-red-400 text-white">Hello world!</h1>;
+  return (
+    <Routes>
+      <Route path=" /" element={<Home />}></Route>
+      <Route path="/books/create " element={<CreateBooks />}></Route>
+      <Route path="/books/details/:id " element={<Showbooks />}></Route>
+      <Route path=" /books/edit/:id" element={<EditBook />}></Route>
+      <Route path="/books/delete/:id " element={<DeleteBooks />}></Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/Showbooks.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Showbooks = () => {
+  return <div></div>;
+};
+
+export default Showbooks;
